dca6157ef8f926d8f1849b3264b14b0707d927cb
Mathias Ertl <mati@fsinf.at>
2010-09-30 10:16:55 +0000

    include setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='RestAuthClient',
+	version='1.0',
+	description='RestAuth client library',
+	author='Mathias Ertl',
+	author_email='mati@fsinf.at',
+	url='http://fs.fsinf.at/wiki/RestAuth',
+	packages=['RestAuthClient']
+)
