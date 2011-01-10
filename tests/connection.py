2fcdf296aeec79d9e65d6de7bf0dd0671b24c296
Mathias Ertl <mati@fsinf.at>
2011-01-10 15:59:05 +0000

    add some minor connection tests

---
+++ tests/connection.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import sys, unittest
+from RestAuthClient.common import RestAuthConnection
+from RestAuthClient.errors import *
+from RestAuthClient import restauth_user
+
+from RestAuthCommon.handlers import content_handler
+
+rest_host = 'http://localhost:8000'
+rest_user = 'vowi'
+rest_passwd = 'vowi'
+
+# all paths in the restauth specification
+paths = [ '/users/', '/users/foo/', '/users/foo/props/', '/users/foo/props/bar',
+	'/groups/', '/groups/foo/', 
+	'/groups/foo/users/',  '/groups/foo/users/bar/', 
+	'/groups/foo/groups/', '/groups/foo/groups/bar' ]
+
+class wrongContentHandler( content_handler ):
+	mime = 'wrong/mime'
+
+class BasicTests( unittest.TestCase ):
+	"""
+	Make some tests directly using the connection. The point is that this
+	way we can test some code that would otherwise never get tested.
+	"""
+
+	def setUp( self ):
+		self.conn = RestAuthConnection( rest_host, rest_user, rest_passwd )
+
+	def tearDown( self ):
+		pass
+	
+	def test_wrongCredentials( self ):
+		conn = RestAuthConnection( rest_host, 'wrong', 'credentials' )
+		for path in paths:
+			try:
+				conn.get( path )
+				self.fail( msg="%s did not require authorization!"%path)
+			except Unauthorized as e:
+				pass
+
+	def test_badRequestPost( self ):
+		try:
+			self.conn.post( paths[0], {'bad':'request'} )
+			self.fail()
+		except BadRequest:
+			pass
+
+	def test_badRequestPut( self ):
+		# we need to create it first, otherwise we might get a 404 instead
+		user = restauth_user.create( self.conn, 'testuser', 'password' )
+		try:
+			self.conn.put( '/users/testuser/', {'bad':'request'} )
+			self.fail()
+		except BadRequest:
+			pass
+
+		user.remove()
+		
+	def test_NotAcceptable( self ):
+		self.conn.set_content_handler( wrongContentHandler() )
+
+		try:
+			self.conn.get( '/users/' )
+			self.fail()
+		except NotAcceptable:
+			pass
+
+	def test_UnsupportedMediaTypePost( self ):
+		self.conn.set_content_handler( wrongContentHandler() )
+
+		try:
+			self.conn.post( '/users/', {'foo': 'bar'} )
+			self.fail()
+		except UnsupportedMediaType:
+			pass
+	
+	def test_UnsupportedMediaTypePut( self ):
+		# we need to create it first, otherwise we might get a 404 instead
+		user = restauth_user.create( self.conn, 'testuser', 'password' )
+		self.conn.set_content_handler( wrongContentHandler() )
+
+		try:
+			self.conn.put( '/users/testuser/', {'foo': 'bar'} )
+			self.fail()
+		except UnsupportedMediaType:
+			pass
+		finally:
+			user.remove()
