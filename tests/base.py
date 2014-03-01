427e04c40134d680b52aa74fae497001214c55c7
Mathias Ertl <mati@fsinf.at>
2014-03-01 20:23:23 +0100

    fix default mime type

--- tests/base.py
+++ tests/base.py
@@ -10,6 +10,6 @@ from RestAuthClient.common import RestAuthConnection
 
 class RestAuthClientTestCase(unittest.TestCase):
     def setUp(self):
-        mime_type = os.environ.get('MIME', 'application_json')
+        mime_type = os.environ.get('MIME', 'application/json')
         self.conn = RestAuthConnection('http://[::1]:8000', 'vowi', 'vowi',
                                        content_handler=mime_type)
