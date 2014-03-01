d4bdcf324503b11296b41e3297b1b2b1d8101255
Mathias Ertl <mati@fsinf.at>
2014-03-01 20:17:55 +0100

    add base class for test cases

---
+++ tests/base.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import os
+import unittest
+
+from RestAuthClient.common import RestAuthConnection
+
+
+class RestAuthClientTestCase(unittest.TestCase):
+    def setUp(self):
+        mime_type = os.environ.get('MIME', 'application_json')
+        self.conn = RestAuthConnection('http://[::1]:8000', 'vowi', 'vowi',
+                                       content_handler=mime_type)
