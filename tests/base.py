83ca85936cfd205888b9e697bfacf3c59a0095be
Mathias Ertl <mati@er.tl>
2014-09-05 22:28:30 +0200

    use new standard username

--- tests/base.py
+++ tests/base.py
@@ -11,5 +11,5 @@ from RestAuthClient.common import RestAuthConnection
 class RestAuthClientTestCase(unittest.TestCase):
     def setUp(self):
         mime_type = os.environ.get('MIME', 'application/json')
-        self.conn = RestAuthConnection('http://[::1]:8000', 'vowi', 'vowi',
+        self.conn = RestAuthConnection('http://[::1]:8000', 'example.com', 'nopass',
                                        content_handler=mime_type)
